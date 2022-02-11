a5e9753378573e85fef9974ec48f0e00dd8b03c4
Akshay Priyadarshi <akshayp1904@outlook.com>
2022-02-11 16:59:59 +0530

    fixed some warnings

--- src/main/java/com/technocrats/workboxutility/WorkboxUtilityApplication.java
+++ src/main/java/com/technocrats/workboxutility/WorkboxUtilityApplication.java
@@ -1,13 +1,11 @@
 package com.technocrats.workboxutility;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.crypto.dsig.XMLObject;
 
 @SpringBootApplication
 @RestController
@@ -32,7 +30,7 @@ public class WorkboxUtilityApplication {
 			String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + XML.toString(jsonObject);
 			return xml;
 		}catch(Exception e){
-			System.out.println(e.toString());
+			System.out.println(e.getMessage());
 			return e.toString();
 		}
 	}
@@ -50,7 +48,7 @@ public class WorkboxUtilityApplication {
 			String jsonString = json.toString(4);
 			return jsonString;
 		}catch(Exception e){
-			System.out.println(e.toString());
+			System.out.println(e.getMessage());
 			return e.toString();
 		}
 	}
