c3482e34bb8b01a614b3a834c47bbc8f7f22cff8
Akshay Priyadarshi <akshayp1904@outlook.com>
2022-02-11 16:56:13 +0530

    Added json2xml and xml2json

--- src/main/java/com/technocrats/workboxutility/WorkboxUtilityApplication.java
+++ src/main/java/com/technocrats/workboxutility/WorkboxUtilityApplication.java
@@ -1,10 +1,13 @@
 package com.technocrats.workboxutility;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.crypto.dsig.XMLObject;
 
 @SpringBootApplication
 @RestController
@@ -15,6 +18,43 @@ public class WorkboxUtilityApplication {
 	public String sayHello(@PathVariable("name") String name) {
 		return String.format("Hello %s!", name);
 	}
+
+	/**
+	 * @name JSON TO XML
+	 * @param body Request Body
+	 * @return XML String
+	 * @author Akshay Priyadarshi
+	 */
+	@PostMapping("/json2xml")
+	public String jsonToXML(@RequestBody() String body){
+		try {
+			JSONObject jsonObject = new JSONObject(body);
+			String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + XML.toString(jsonObject);
+			return xml;
+		}catch(Exception e){
+			System.out.println(e.toString());
+			return e.toString();
+		}
+	}
+
+	/**
+	 * @name XML TO JSON
+	 * @param body Request Body
+	 * @return JSON String
+	 * @author Akshay Priyadarshi
+	 */
+	@PostMapping("/xml2json")
+	public String xmlToJson(@RequestBody() String body){
+		try {
+			JSONObject json = XML.toJSONObject(body);
+			String jsonString = json.toString(4);
+			return jsonString;
+		}catch(Exception e){
+			System.out.println(e.toString());
+			return e.toString();
+		}
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkboxUtilityApplication.class, args);
